d47150a8ff112cb91372fca6221700e00b1b632f
sunnykhatter <lakshaykhatter@gmail.com>
2013-07-08 13:15:08 -0400

    Finishing step 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Photogur::Application.routes.draw do
 
+  post 'pictures' => "pictures#create"
 
   get 'pictures' => 'pictures#index'
   
+  get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show', as: "picture"
+
 
 end
 
